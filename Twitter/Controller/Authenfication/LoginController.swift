0c3a6c0246c6606e959b8ac22bade5b9a86467e3
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-30 01:16:00 +0300

    Update RegistrationController

--- Twitter/Controller/Authenfication/LoginController.swift
+++ Twitter/Controller/Authenfication/LoginController.swift
@@ -24,7 +24,22 @@ class LoginController: UIViewController {
         button.setTitleColor(.twitterBlue, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.layer.cornerRadius = 12
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
+                                                        attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+                                                                     NSAttributedString.Key.foregroundColor : UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Sigh Up", attributes:
+                                                    [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16),
+                                                     NSAttributedString.Key.foregroundColor : UIColor.white]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(sighUp), for: .touchUpInside)
+        return button
+        
     }()
     
     // MARK: - Lifecycle
@@ -34,6 +49,16 @@ class LoginController: UIViewController {
         
     }
     // MARK: - Selectors
+    
+    @objc func handleLogin() {
+
+    }
+    
+    @objc func sighUp() {
+        let registarationVC = RegistationController()
+        navigationController?.pushViewController(registarationVC, animated: true)
+    }
+
     
     // MARK: - Helpers
     
@@ -48,11 +73,14 @@ class LoginController: UIViewController {
         view.addSubview(emailContainer)
         emailContainer.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
         view.addSubview(passwordContainer)
-        passwordContainer.anchor(top: emailContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 8, paddingLeft: 16, paddingRight: 16, height: 50)
+        passwordContainer.anchor(top: emailContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
         passwordContainer.textField.isSecureTextEntry = true
         
         view.addSubview(loginButton)
-        loginButton.anchor(top: passwordContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 16,paddingRight: 16, height: 50)
+        loginButton.anchor(top: passwordContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16,paddingRight: 16, height: 50)
+        
+        view.addSubview(signUpButton)
+        signUpButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,height: 50)
         
     }
 }
--- Twitter/Controller/Authenfication/RegistationController.swift
+++ Twitter/Controller/Authenfication/RegistationController.swift
@@ -9,7 +9,42 @@ import UIKit
 
 class RegistationController: UIViewController {
     // MARK: - Properties
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView(image: UIImage(systemName: "photo"))
+        iv.tintColor = .white
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
     
+    let emailContainer = TextFieldView(placeholder: "Email", image: "envelope")
+    let passwordContainer = TextFieldView(placeholder: "Password", image: "lock")
+    let fullnameContainer = TextFieldView(placeholder: "Full Name", image: "person.crop.circle.badge.checkmark")
+    let usernameContainer = TextFieldView(placeholder: "Username", image: "person.crop.circle")
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sigh Up", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.layer.cornerRadius = 12
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Alredy have an account? ",
+                                                        attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+                                                                     NSAttributedString.Key.foregroundColor : UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Log In", attributes:
+                                                    [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16),
+                                                     NSAttributedString.Key.foregroundColor : UIColor.white]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+//        button.addTarget(self, action: #selector(sighUp), for: .touchUpInside)
+        return button
+        
+    }()
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +55,25 @@ class RegistationController: UIViewController {
     // MARK: - Helpers
     
     func configureUI() {
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .twitterBlue
+        view.addSubview(logoImageView)
+        view.addSubview(emailContainer)
+        view.addSubview(passwordContainer)
+        view.addSubview(fullnameContainer)
+        view.addSubview(usernameContainer)
+        view.addSubview(loginButton)
+        view.addSubview(signUpButton)
+
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 50)
+        logoImageView.setDimensions(width: 200, height: 200)
+        
+        emailContainer.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
+        passwordContainer.anchor(top: emailContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
+        fullnameContainer.anchor(top: passwordContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
+        usernameContainer.anchor(top: fullnameContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16, height: 50)
+        signUpButton.anchor(top: usernameContainer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 16,paddingRight: 16, height: 50)
+        loginButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,height: 50)
+        
+        
     }
 }
